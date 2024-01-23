4e112b7519f85f29ac225ef1ca3138ef28345c9a
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-01-24 06:59:17 +0700

    Add files via upload
    Wed 24 Jan 06.59

---
+++ ex3.py
@@ -0,0 +1,21 @@
+print("I will now count my chicken:")
+
+print("Hens", 25 + 30 / 6) #The comma after "Hens" is used to separate string and numerical processing
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+print(3 + 2 < 5 - 7) #Will gave a False answer because 5 is greater than -2
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
---
+++ ex3drill.py
@@ -0,0 +1,8 @@
+#Formula to know cubes volume
+print("The volume of a cube with side lenght 7 is", 7 ** 3,"cm^3")
+
+#Formula to know a Ball volume
+print("The volume of a ball with radius 9 is", (4 / 3) * 3.14 * 9 ** 3, "cm^3")
+
+#Formula to know a pyramid volume
+print("The volume of a pyramid with base 6 and height 9.5 is", (6 ** 2 * 9.5) / 3, "cm^3")
