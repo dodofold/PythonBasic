d7630de8a038d5df83f4c33a8e4b381f49e671f5
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-01-24 23:47:00 +0700

    Add files via upload
    Wed, January 24 2024, 1146

---
+++ ex5.py
@@ -0,0 +1,24 @@
+name = 'Zed A. Shew'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # Lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's got {weight} pound heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+#Student Drill!
+
+height_cm = height * 2.54
+weight_kg = weight * 0.453592
+
+print(f"He's {height_cm} cm tall.")
+print(f"He's {weight_kg} kg heavy.")
+
+#End in here!
