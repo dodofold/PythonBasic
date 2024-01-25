e8e62bddca7d1fe7093e347b37b31be469d799a7
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-01-25 16:00:15 +0700

    Exercise 8 and 9 from LPTHW book
    Thursday, January 25 2024 1600

---
+++ ex9.py
@@ -0,0 +1,14 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun" #Assign a string to variable days
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #Assign a string to variable months
+
+print("Here are the days: ", days) #Printing string + variable
+print("Here are the months: ", months) #Printing string + variable
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")    #Printing with triple double-quotes
