0bd8d1679bf38643242fc59c0a47264fb9deec89
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-01-27 00:33:51 +0700

    Update ex11drill.py
    Add comments to some line

--- ex11drill.py
+++ ex11drill.py
@@ -3,10 +3,12 @@ likes = input()
 print("Where do you live?", end=' ')
 lives = input()
 print("What kind of computer do you have?", end=' ')
-computer = input()
+computer = input() 
 
 print(f"""
 Alright, so you said {likes} about liking me.
 You live in {lives}. Not sure where that is.
 And you have a {computer} computer. Nice.
-""")
+""") #assign a string to variable
+
+# End in here!
