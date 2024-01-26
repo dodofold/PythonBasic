6d19080bfe500b4302782f1e0ebadaef964cc090
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-01-26 20:30:47 +0700

    Exercise 11 and student drill from LPTHW
    Friday, 26 January 2024

---
+++ ex11drill.py
@@ -0,0 +1,12 @@
+print("What do you like?", end='')
+likes = input()
+print("Where do you live?", end=' ')
+lives = input()
+print("What kind of computer do you have?", end=' ')
+computer = input()
+
+print(f"""
+Alright, so you said {likes} about liking me.
+You live in {lives}. Not sure where that is.
+And you have a {computer} computer. Nice.
+""")
