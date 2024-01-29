fee7d7ed8f64d4f842ac39f6fa7aa062ba0885c9
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-01-30 06:18:00 +0700

    Exercise 16 from LPTHW book with drills update
    Tuesday, January 30, 2024 617

--- ex16.py
+++ ex16.py
@@ -22,12 +22,8 @@ line3 = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f"{line1}\n{line2}\n{line3}")
+
 
 print("And finally we close it.")
 target.close()
