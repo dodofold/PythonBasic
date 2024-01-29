e57476e65a897924a36d2254e69515dfa87f0fe3
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-01-29 21:59:28 +0700

    Exercise 12 & 13

---
+++ ex13drill.py
@@ -0,0 +1,12 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, name, degree, skill = argv
+
+print("The script is called:", script)
+print("Your name is:", name)
+print("Your are a ", degree, "degree")
+print("Your have a", skill, "skill")
+
+input("Press Enter to continue...")
+likes = input("What do you like? ")
+print("So you had a", likes, "in your life and the you get your", degree,"degree.")
