1b0c25c2dc7313c970a60111a1ba228229b61c4f
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-02-28 21:47:54 +0700

    Exercise 26 from LPTHW book
    Wednesday, 28 February 2024 2147

--- ex26.py
+++ ex26.py
@@ -1,33 +1,44 @@
 from sys import argv
+#from os.path import exists
 
-print("How old are you?", end24=' ')
+print('What is your name?', end=' ')
+name = input()
+print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
 height = input()
 print("How much do you weigh?", end=' ')
 weight = input()
 
-print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print(f"So, your name is {name}, you're {age} years old, {height} cm tall and {weight} kg heavy.\n")
 
-print("After this you need to open ### Open file and print")
+print("After this you need to open file and print\n")
 
-filename = argv
+# Opening test.txt
+
+script, filename = argv
 
 txt = open(filename)
-print("Here's your file {filename}:")
+print(txt.read())
+
+print(f"Here's your file {filename}:")
 print(txt.read())
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
+indata = txt_again.read()
 
-print(txt_again())
-###
+print(txt_again.read())
+print(f"\nThe input file is {len(indata)} bytes long")
+txt.close()
+txt_again.close()
+#
 
 
-print('Let\'s practice everything.')
-print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.')
+print('\nLet\'s practice everything.')
+print('You\'d need to know \'bout escapes with \\ that do \n newlines and \t tabs.\n')
 
 poem = """
 \tThe lovely world
@@ -40,11 +51,11 @@ and requires an explanation
 
 print("--------------")
 print(poem)
-print("--------------")
+print("--------------\n")
 
 
 five = 4 - 2 + 4 - 1
-print(f"This should be five: {five}")
+print(f"This should be five: {five}\n")
 
 def secret_formula(started):
     jelly_beans = started * 500
@@ -54,38 +65,38 @@ def secret_formula(started):
 
 
 start_point = 10000
-beans, jars = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point)
 
 # remember that this is another way to format a string
-print("With a starting point of: {}".format(start_point))
+print("\nWith a starting point of: {}".format(start_point))
 # it's just like with an f"" string
 print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 
 people = 20
-cates = 30
+cats = 30
 dogs = 15
 
 
 if people < cats:
-    print ("Too many cats! The world is doomed!")
+    print ("\n\nToo many cats! The world is doomed!")
 
 if people > cats:
     print("Not many cats! The world is saved!")
 
 if people < dogs:
-    print("The world is drooled on!")
+    print("Too many dogs! The world is drooled on!")
 
 if people > dogs:
-    print("The world is dry!")
+    print("Too many people! The world is dry!")
 
 
 dogs += 5
@@ -95,7 +106,6 @@ if people >= dogs:
 
 if people <= dogs:
     print("People are less than or equal to dogs.")
-
 
 if people == dogs:
     print("People are dogs.")
