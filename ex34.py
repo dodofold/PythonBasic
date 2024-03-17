e66a1011d64a2290776b8fe12d592e13fad07d26
Rafiq A. Widodo <66117286+dodofold@users.noreply.github.com>
2024-03-17 21:49:29 +0700

    Exercise 34 from LPTHW Book
    Sunday, March 17 2024 2149

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+print(f"The animal at 1 is {animals[1]}")
+print(f"The third animal is {animals[2]}")
+print(f"The first animal is {animals[0]}")
+print(f"The animal at 3 is {animals[3]}")
+print(f"The fifth animal is {animals[4]}")
+print(f"The animal at 2 is {animals[2]}")
+print(f"The sixth animal is {animals[5]}")
+print(f"The animal at 4 is {animals[4]}")
